27f15e0fed46ed9cd7f8a709a306126349e5f143
SterDream <130822110+SterDream@users.noreply.github.com>
2026-03-14 01:53:42 +0900

    Add files via upload

---
+++ F0_MB_iSTFT_VITS/text/__init__.py
@@ -0,0 +1,23 @@
+from phonemize import Tokenizer
+converter = Tokenizer()
+
+def text_to_sequence(text):
+  '''Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
+    Args:
+      text: string to convert to a sequence
+      cleaner_names: names of the cleaner functions to run the text through
+    Returns:
+      List of integers corresponding to the symbols in the text
+  '''
+  x, x_lengths, lang = converter.to_tensor(text)
+  return x, x_lengths, lang
+
+
+def infer_g2p(text):
+  x, x_lengths, lang = converter.to_tensor(text)
+  return x, x_lengths, lang
+
+
+if __name__ == "__main__":
+  print(text_to_sequence("吾輩は猫である。"))
+  print(infer_g2p("吾輩は猫である。"))
