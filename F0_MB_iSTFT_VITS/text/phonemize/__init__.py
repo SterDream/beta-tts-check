31e9be2ebfc86f722c42725aadf7b12442d118b1
SterDream <130822110+SterDream@users.noreply.github.com>
2026-03-15 03:04:04 +0900

    Delete F0_MB_iSTFT_VITS directory

--- F0_MB_iSTFT_VITS/text/phonemize/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Chinese_Phonemizer import G2P_Chinese_to_Phoneme
-from .Korean_Phonemizer import G2P_Korean_to_Phoneme
-from .Japanese_Phonemizer import G2P_Japanese_to_Phoneme
-from .En_cs_de_es_fa_fr_it_lb_nl_ru_sv_sw_Phonemizer import G2P_multilang_to_Phoneme, G2P_US_English_to_Phoneme
-from .IPA_tokenizer import Tokenizer
-from .to_IPA_converter import *
-from .symbols import symbols
